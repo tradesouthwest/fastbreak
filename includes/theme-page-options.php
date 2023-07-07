26f8a1389780daed70bb5fc1bfaa857b54fafa10
tradesouthwest <tradesouthwest@gmail.com>
2023-07-07 14:21:44 -0700

    fixed dile

--- includes/theme-page-options.php
+++ includes/theme-page-options.php
@@ -4,7 +4,7 @@
  */
 
 // A1
-add_action( 'init', 'fastbreak_theme_customizer_css' );  
+add_action( 'wp_enqueue_scripts', 'fastbreak_theme_customizer_css', 15 );  
 // A2
 add_action( 'admin_menu',         'fastbreak_theme_options_help_page' );
 
@@ -33,41 +33,31 @@ function fastbreak_theme_options_help_page() {
 */
 function fastbreak_theme_customizer_css() 
 {   
-    $fnt = '';
+    $fnt = $css = $font = '';
     $tsb  = ( empty( get_theme_mod( 'fastbreak_topbars_color' ) ) ) ? '#3d3d3d' 
-               : get_theme_mod( 'fastbreak_topbars_color' );
+                   : get_theme_mod( 'fastbreak_topbars_color' );
     $fnt  = ( empty( get_theme_mod( 'fastbreak_font_choices' ) ) ) ? 'montserrat' 
-               : get_theme_mod( 'fastbreak_font_choices' );
+                   : get_theme_mod( 'fastbreak_font_choices' );
+    $urib  = get_stylesheet_directory_uri() . '/rels/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCtr6Hw5aXo.woff2';
     
     if( $fnt == 'initial' ) {
-
-        //$font = '<style id="fastbreak-fonts-style" type="text/css">';
-        $font = 'body, button, input, select, textarea, .h1{';
-        $font = 'font-family: initial;}';
-
+        $font .= 'body, button, input, select, textarea, .h1{';
+        $font .= 'font-family: initial;}';
     } 
     if( $fnt == 'montserrat' ) { 
-
-        $urib  = get_stylesheet_directory_uri() . '/rels/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCtr6Hw5aXo.woff2';
-        //$font = '<style id="fastbreak-montserrat-style" type="text/css">';
-        $font = 
-        "@font-face {font-family: 'Montserrat';font-style: normal;font-weight: 400;src: url( $urib ) format('woff2');
+        $font .= "@font-face {font-family: 'Montserrat';font-style: normal;font-weight: 400;src: url( $urib ) format('woff2');
         unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
         }";
         $font .= 'body, button, input, select, textarea, .h1{';
         $font .= 'font-family: "Montserrat";}';
     }
     if( $fnt == 'arial' ) { 
-
         $font = '';
     }
-
     /* use above set values into inline styles */
-    //$css = '<style id="fastbreak-inline-customizer" type="text/css">';
-    $css = '#footerCopy,.top-strip,.nav{background-color: ' . esc_attr($tsb) . '}' . $font;
-    //$css .= '</style>' . $font;
-
-    wp_register_style( 'fastbreak-inline-customizer', false );
+    $css .= $font . ' #footerCopy,.top-strip,.nav{background-color: ' . esc_attr($tsb) . '}';
+    
+    wp_register_style( 'fastbreak-inline-customizer', true );
 	wp_enqueue_style( 'fastbreak-inline-customizer' );
 	wp_add_inline_style( 'fastbreak-inline-customizer', $css );
         
